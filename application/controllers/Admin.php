6280ae116e5517701a917d8646d583a5fc98b4cb
MrBercisk <bimosatrio814@gmail.com>
2022-05-22 01:26:12 +0700

    Web 3 Pert 7

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -6,6 +6,7 @@ class Admin extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        $this->load->model(['ModelBuku', 'ModelUser', 'ModelBooking']);
         cek_login();
         cek_user();
     }
@@ -18,8 +19,8 @@ class Admin extends CI_Controller
         $data['buku'] = $this->ModelBuku->getLimitBuku()->result_array();
 
         //mengupdate stok dan dibooking pada tabel buku
-        $detail = $this->db->query("SELECT*FROM booking,booking_detail WHERE DAY(curdate()) < 
-        DAY(batas_ambil) AND booking.id_booking=booking_detail.id_booking")->result_array();
+        $detail = $this->db->query("SELECT*FROM booking,booking_detail, buku WHERE 
+        booking.id_booking=booking_detail.id_booking and booking_detail.id_buku=buku.id")->result_array();
         foreach ($detail as $key) {
             $id_buku = $key['id_buku'];
             $batas = $key['tgl_booking'];
@@ -29,24 +30,24 @@ class Admin extends CI_Controller
             if ($beda->days > 2) {
                 $this->db->query("UPDATE buku SET stok=stok+1, dibooking=dibooking-1 WHERE 
                 id='$id_buku'");
+            }
+        }
+
+        //menghapus otomatis data booking yang sudah lewat dari 2 hari
+        $booking = $this->ModelBooking->getData('booking');
+        if (!empty($booking)) {
+            foreach ($booking as $bo) {
+                $id_booking = $booking->id_booking;
+                $tglbooking = $booking->tgl_booking;
+                $tglawal = date_create($tglbooking);
+                $tglskrg = date_create();
+                $beda = date_diff($tglawal, $tglskrg);
+                if ($beda->days > 2) {
+                    $this->db->query("DELETE FROM booking WHERE id_booking='$id_booking'");
+                    $this->db->query("DELETE FROM booking_detail WHERE id_booking='$id_booking'");
                 }
             }
-            
-            //menghapus otomatis data booking yang sudah lewat dari 2 hari
-            $booking = $this->ModelBooking->getData('booking');
-            if (!empty($booking)) {
-                foreach ($booking as $bo) {
-                    $id_booking = $booking->id_booking;
-                    $tglbooking = $booking->tgl_booking;
-                    $tglawal = date_create($tglbooking);
-                    $tglskrg = date_create();
-                    $beda = date_diff($tglawal, $tglskrg);
-                    if ($beda->days > 2) {
-                        $this->db->query("DELETE FROM booking WHERE id_booking='$id_booking'");
-                        $this->db->query("DELETE FROM booking_detail WHERE id_booking='$id_booking'");
-                    }
-                }
-            }
+        }
 
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar', $data);
@@ -54,5 +55,4 @@ class Admin extends CI_Controller
         $this->load->view('admin/index', $data);
         $this->load->view('templates/footer');
     }
-
 }
